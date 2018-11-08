22c13ef5c3f6bf433ed0cf5cb17b09a1287168ae
global-blockchain <admin@imonero.org>
2018-11-08 13:50:04 +0800

    test logger

--- simplechaincode.go
+++ simplechaincode.go
@@ -22,7 +22,6 @@ func (t *simplechaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response{
 	var b_parm = args[1]
 	var c_parm = args[2]*/
 	var logger = shim.NewLogger("simplechaincode")
-	logger.Info("================================================just for test================================")
 	logger.Warning("----------------------------------test Warning--------------------------------------------")
 	/*logger.info("args0:%s",a_parm)
 	logger.info("args1:%s",b_parm)
