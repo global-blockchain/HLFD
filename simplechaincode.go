8bced0890a64c5f39dcb56d2058383cf1e342c2d
global-blockchain <admin@imonero.org>
2018-11-08 11:04:14 +0800

    test logger and lookup params

--- simplechaincode.go
+++ simplechaincode.go
@@ -16,6 +16,17 @@ func (t *simplechaincode) Init(stub shim.ChaincodeStubInterface) pb.Response  {
 
 func (t *simplechaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response{
 	fmt.Println("======= Invoke function is call successfully ========")
+
+	_,args := stub.GetFunctionAndParameters();
+	var a_parm = args[0]
+	var b_parm = args[1]
+	var c_parm = args[2]
+	var logger = shim.NewLogger("simplechaincode")
+	logger.info("================================================just for test================================")
+	logger.info("args0:%s",a_parm)
+	logger.info("args1:%s",b_parm)
+	logger.info("args2:%s",c_parm)
+	
 	return shim.Success([] byte("Success invoke"))
 }
 
